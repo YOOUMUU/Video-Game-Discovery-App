743e2a831c24526712a984eaa71d0ca39096fb26
YOOUMUU <yooumuu@gmail.com>
2023-05-02 15:27:40 +0800

    Fetching the Genres

---
+++ src/hooks/useGenres.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import apiClient, { CanceledError } from '../services/api-client';
+
+interface Genre {
+  id: number;
+  name: string;
+}
+
+interface FetchGenresResponse {
+  count: number;
+  results: Genre[];
+}
+
+const useGenres = () => {
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [error, setError] = useState('');
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setLoading(true);
+
+    apiClient
+      .get<FetchGenresResponse>('/genres', { signal: controller.signal })
+      .then((res) => {
+        setLoading(false);
+        setGenres(res.data.results);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setLoading(false);
+        setError(err.message);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { genres, error, isLoading };
+};
+
+export default useGenres;
