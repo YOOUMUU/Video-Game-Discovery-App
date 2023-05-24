d65059b6672a14da966671e456f319213a2af203
YOOUMUU <yooumuu@gmail.com>
2023-05-25 02:14:02 +0800

    Fetching a game

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,7 +7,16 @@ interface Props {
 
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box width="100%" borderRadius={10} overflow="hidden" boxShadow="md">
+    <Box
+      width="100%"
+      borderRadius={10}
+      overflow="hidden"
+      boxShadow="md"
+      _hover={{
+        transform: 'scale(1.03)',
+        transition: 'transform .2s ease-in',
+      }}
+    >
       {children}
     </Box>
   );
